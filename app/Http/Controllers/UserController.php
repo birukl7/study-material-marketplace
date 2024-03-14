20b32885d12c233e9e0b125acba04e2468cf531a
Dechie <dechu373@protonmail.com>
2024-03-14 08:29:03 +0300

    few

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
 
 class UserController extends Controller
 {
@@ -13,6 +16,26 @@ class UserController extends Controller
 
     public function store(Request $request) {
 
+        $validatedData = $request->validate([
+            'username' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+
+        // Create a new user using the validated data
+        $user = User::create([
+            'username' => $validatedData['username'],
+            'email' => $validatedData['email'],
+            'password' => bcrypt($validatedData['password']),
+        ]);
+
+        dd($user);
+
+        // Authenticate the user
+        Auth::login($user);
+
+        // Redirect the user after successful registration
+        return redirect('/home');
     }
 
     public function login(Request $request) {
